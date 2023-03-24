ad7f75328d1900c332bb5e3dba950c9bacd1aeb3
Blastiq <blastiquemajar@gmail.com>
2023-03-24 23:11:10 +0100

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Card from "./Card";
+import image from "./bas.JPG"
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+
+function createCard(contacts) {
+  return <Card 
+  id={contacts.id}
+  key={contacts.id}
+  name={contacts.name}
+  img={contacts.imgURL}
+  phone={contacts.phone}
+  email={contacts.email}
+  />
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img={image}/>
+      <h1 className="nameColor">Blastiq</h1>
+
+
+      {contacts.map(createCard)}
+
+
+
+
+      {/* <Card 
+      name="Barnabas" 
+      img={image}
+      phone="+123 456 789" 
+      email="b@barnabas.com" />
+      <Card 
+      name={contacts[0].name}
+      img={contacts[0].imgURL}
+      phone={contacts[0].phone} 
+      email={contacts[0].email} />
+      <Card 
+      name={contacts[1].name}
+      img={contacts[1].imgURL}
+      phone={contacts[1].phone} 
+      email={contacts[1].email} />
+      <Card 
+      name={contacts[2].name}
+      img={contacts[2].imgURL}
+      phone={contacts[2].phone} 
+      email={contacts[2].email} />
+      */}
+    </div>
+  );
+} 
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details"
+
+
+function Card(props) {
+    return       <div className="card">
+    <div className="top">
+    <p>{props.id}</p>
+      <h2 className="name">{props.name}</h2>
+      <Avatar img={props.img} />
+    </div>
+    <div className="bottom">
+    <div>
+      <Details 
+      detailsInfo={props.phone}
+       />
+      <Details 
+      detailsInfo={props.email}
+       />
+
+       </div>
+</div>
+  </div>
+}
+
+
+export default Card;
